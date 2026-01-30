074595c491888cf6a2cceeb89a46c8bee0a0b1a4
nibirigusage <nibirigusage@gmail.com>
2026-01-30 06:12:41 +0600

    Add files via upload

---
+++ product_management/index.php
@@ -0,0 +1,24 @@
+﻿<?php
+require_once __DIR__ . "/controller/ProductController.php";
+
+$controller = new ProductController();
+$action = isset($_GET["action"]) ? $_GET["action"] : "index";
+
+switch ($action) {
+    case "add":
+        $controller->add();
+        break;
+    case "edit":
+        $controller->edit();
+        break;
+    case "delete":
+        $controller->delete();
+        break;
+    case "search":
+        $controller->search();
+        break;
+    case "index":
+    default:
+        $controller->index();
+        break;
+}
